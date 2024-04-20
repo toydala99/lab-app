53a28e616714afd0eb745121ca2975f00702bf00
toydala99 <antoniodala83@gmail.com>
2024-04-20 08:03:46 +0100

    html css js

---
+++ app.js
@@ -0,0 +1,44 @@
+/*
+O objetivo deste desafio é criar uma aplicação que liste as infraestruturas
+de uma escola, permitindo a classificação de laboratórios ou salas de aulas
+como funcionais e não funcionais, Ao final, a aplicação deve cadastrar /
+actualizar o total de laboratórios e salas de aulas funcionais e não
+funcionais.
+
+
+[x] Defina uma estrutura de dados de sua escolha para armazenar os
+dados de acordo com a interface apresentada no Figma.
+
+[ ] Os campos de entrada (inputs) não devem aceitar números negativos.
+
+[ ] Para a primeira linha (Sala de Aula Teóricas), os campos de entrada
+(Funcionais e Não Funcionais) devem estar bloqueados com valores 0.
+
+[ ] Caso já existam dados na estrutura de dados definida, deve ser possível actualizá-los.
+
+[x] Escolha qualquer framework com o qual você se sinta confortável em trabalhar.
+Este desafio não tem preferência por um específico, portanto escolha aquele com o
+qual você se sentirá mais confiante para apresentar e discutir durante a entrevista ;)
+
+*/
+
+/*
+Plano de execução
+
+- Estrutura de dados: Array e salvar no localstorage
+pk? Assim poderei demonstrar que conheço o básico da linguagem e que posteriormente fica fácil
+aprender qualquer outro framework
+
+- Framework: Nenhum, vamos usar HTML, CSS, JS puros!
+pk? O projecto é símples, não precisamos do poder do react ou outro framework para fazer esse desafio
+
+
+Primeiro passo - Criar o visual da página.
+Segundo passo - Criar a estrutura de dados e o seu método update.
+Terceiro passo - Filtar os inputs para não aceitarem números negativos.
+Quarto passo - Enviar o desafio.
+
+*/
+
+let salas = "@toydala99";
+console.log(salas);
